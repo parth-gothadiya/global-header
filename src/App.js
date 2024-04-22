57ac3c718211409466c7dd2767d469a245ebaf8f
parth-gothadiya <parthgothadiya@gmail.com>
2024-04-22 13:17:03 +0530

    one

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Component/Home';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -2,12 +2,23 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import logo from '../Asseat/logo.png'
+import user1 from '../Asseat/user.png'
+import './header.css'
+import { useAuth0 } from '@auth0/auth0-react'
+import { Button } from 'bootstrap'
+
+
+
+
+
+
 
 const navigation = [
     { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
+    { name: 'Category', href: '#', current: false },
+    { name: 'Blog', href: '#', current: false },
+    { name: 'Contact ', href: '#', current: false },
+    { name: 'About', href: '#', current: false },
 ]
 
 function classNames(...classes) {
@@ -15,15 +26,18 @@ function classNames(...classes) {
 }
 
 export default function Header() {
+
+    const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
+
     return (
-        <Disclosure as="nav" className="bg-gray-800">
+        <Disclosure as="nav" className="bg-gray-800  ">
             {({ open }) => (
                 <>
-                    <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+                    <div className="mx-auto max-w-7xl px-2 sm:px-6  lg:px-6 m-0 ">
                         <div className="relative flex h-16 items-center justify-between">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu button*/}
-                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                     <span className="absolute -inset-0.5" />
                                     <span className="sr-only">Open main menu</span>
                                     {open ? (
@@ -36,7 +50,7 @@ export default function Header() {
 
 
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                                <div className="flex flex-shrink-0 items-center">
+                                <div className="flex flex-shrink-0 items-center me-5 ms-0">
                                     <img
                                         className="h-8 w-auto items-center flex"
                                         src={logo}
@@ -50,7 +64,7 @@ export default function Header() {
                                                 key={item.name}
                                                 href={item.href}
                                                 className={classNames(
-                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-dark-700 big1 hover:text-amber-500',
                                                     'rounded-md px-3 py-2 text-sm font-medium'
                                                 )}
                                                 aria-current={item.current ? 'page' : undefined}
@@ -66,7 +80,7 @@ export default function Header() {
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                 <button
                                     type="button"
-                                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                                    className="relative rounded-full  bg-gray-800 p-1 text-white hover:text-amber-500 focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                                 >
                                     <span className="absolute -inset-1.5" />
                                     <span className="sr-only">View notifications</span>
@@ -74,15 +88,22 @@ export default function Header() {
                                 </button>
 
                                 {/* Profile dropdown */}
-                                <Menu as="div" className="relative ml-3">
+                                {/* {
+                                    isAuthenticated ? (
+                                        <Button type="Login" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Logout</Button>
+                                    ) :
+                                        (
+                                            <Button type="Login" onClick={() => loginWithRedirect()}>Login</Button>
+                                        )} */}
+                                <Menu as="div" className="relative ml-5">
                                     <div>
-                                        <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                                        <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                                             <span className="absolute -inset-1.5" />
                                             <span className="sr-only">Open user menu</span>
                                             <img
                                                 className="h-8 w-8 rounded-full"
-                                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                alt=""
+                                                src={user1}
+                                                alt="user"
                                             />
                                         </Menu.Button>
                                     </div>
@@ -100,7 +121,7 @@ export default function Header() {
                                                 {({ active }) => (
                                                     <a
                                                         href="#"
-                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-dark hover:text-amber-500 hover:font-semibold')}
                                                     >
                                                         Your Profile
                                                     </a>
@@ -110,7 +131,7 @@ export default function Header() {
                                                 {({ active }) => (
                                                     <a
                                                         href="#"
-                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-dark  hover:text-amber-500 hover:font-semibold')}
                                                     >
                                                         Settings
                                                     </a>
@@ -119,8 +140,8 @@ export default function Header() {
                                             <Menu.Item>
                                                 {({ active }) => (
                                                     <a
-                                                        href="#" 
-                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                        href="#"
+                                                        className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-dark  hover:text-amber-500 hover:font-semibold')}
                                                     >
                                                         Sign out
                                                     </a>
