0dd1dd220abfbf8b640582c6c94bd0b5cff957fc
parth-gothadiya <parthgothadiya@gmail.com>
2024-04-23 11:58:38 +0530

    about

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Component/Home';
 import Contact from './Component/Contact';
 import Feature from './Component/Feature';
 import Category from './Component/Category';
+import About from './Component/About';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -16,6 +17,7 @@ function App() {
       <Route path='/Contact' element={<Contact/>}/>
       <Route path='/Category' element={<Category/>}/>
       <Route path='/Feature' element={<Feature/>}/>
+      <Route path='/About' element={<About/>}/>
     </Routes>
 
   
