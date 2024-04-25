11f99f561f21d0d9c26176ddc73283f3b7dbd0ee
parth-gothadiya <parth25802580@gmail.com>
2024-04-25 15:32:30 +0530

    up

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -181,7 +181,7 @@ export default function Header() {
                                     <a
                                         href="#"
                                         onClick={() => loginWithRedirect()}
-                                        className="rounded-md bg-gray-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:text-black hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 button">
+                                        className="rounded-md bg-amber-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:text-black hover:bg-amber-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 button">
                                         Login
                                     </a>
 
