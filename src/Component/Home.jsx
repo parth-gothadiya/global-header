46201e5c7c530b8a866029e40dda2ec632184577
parth-gothadiya <parth25802580@gmail.com>
2024-04-25 15:42:26 +0530

    up

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Header from './Header';
 import Footer from './footer';
 import Section1 from './Section1';
+import Theam from './Theam';
+import Section2 from './Section2';
 
 function Home() {
   return (
@@ -12,21 +14,12 @@ function Home() {
 
         <Header />
         <Section1 />
-        <Footer />
-        <div className="fixed inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80 fixed "
-          aria-hidden="true">
-          <div className="relative  left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }} />
+      <Section2/>
 
-          <div className="relative  left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/1 rotate-[10deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }} />
-        </div>
+
+      
+        <Footer />
+        <Theam/>
       </div>
     </>
 
